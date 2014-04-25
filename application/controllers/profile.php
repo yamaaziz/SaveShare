bfe6f82dc66e0dffa4cd2a073c07ab2f4c15b1c8
sossocode <sofia.balter@hotmail.se>
2014-04-25 10:10:28 +0200

    25 april

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -123,13 +123,10 @@ class Profile extends CI_Controller{
     	if($this->search_model->adv_search()){
            		
            // Visa en resultatlista med användare
-           $this->load->view('templates/header');
-		   $this->load->view('profile/search/search_result', $data1);
-		   $this->load->view('templates/footer');
+           //$this->load->view('templates/header');
+		   //$this->load->view('profile/search/search_result', $data1);
+		   //$this->load->view('templates/footer');
            
-           
-           
-           //redirect('profile');
            $data1['user_info']		= 	$this->collect_user();
 		   $data1['economy_info']	=	$this->collect_economy();
     		//Load Views
@@ -140,7 +137,7 @@ class Profile extends CI_Controller{
            }
            else
            {
-           
+           //Göra nått?
            }
            
            }
