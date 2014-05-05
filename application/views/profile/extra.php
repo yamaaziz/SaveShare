97c17cec9447908fb1a9432cc46e4e462012915a
sossocode <sofia.balter@hotmail.se>
2014-05-05 10:45:41 +0200

    5maj10.45

--- application/views/profile/extra.php
+++ application/views/profile/extra.php
@@ -4,9 +4,15 @@
 	<?php $var = get_object_vars($user_info);?>
 
 	<div class="btn-group">
-<button type="button" class="btn btn-warning btn-lg">
+<button type="button" class="btn btn-warning btn-lg" data-toggle="button"> 
   <span class="glyphicon glyphicon-star"></span> Follow
+  
 </button>
+
+
+
+
+
 <button type="button" class="btn btn-info btn-lg">
   <span class="glyphicon glyphicon-pencil"></span> Send message
 </button>
