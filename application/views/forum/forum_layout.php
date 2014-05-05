a94cda71666619680bf39f0ee6143c7624ffa81a
sossocode <sofia.balter@hotmail.se>
2014-05-05 16:19:44 +0200

    5maj

--- application/views/forum/forum_layout.php
+++ application/views/forum/forum_layout.php
@@ -19,6 +19,101 @@
 		            
 		        </div>
 		    </div>
+		    
+		    		<div class="col-lg-4">  
+		  <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            The topic you want to discuss
+                        </div>
+                        <div class="panel-body">
+                            <p>Here you can write about economy.</p>
+                        </div>
+                         <div class="panel-footer">
+                            <div class="input-group">
+                                <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-warning btn-sm" id="btn-chat">
+                                        Send
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+		</div>
+		
+		<div class="panel-body">
+                            <div id="morris-area-chart"></div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> Forum
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Actions
+                                        <span class="caret"></span>
+                                    </button>
+                                    
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered table-hover table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Topic</th>
+                                                    <th>Author</th>
+                                                    <th>Start date</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>3326</td>
+                                                    <td>Investing in shares</td>
+                                                    <td>Sofia</td>
+                                                    <td>17/11/2013</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3325</td>
+                                                    <td>PPM savings</td>
+                                                    <td>Johanna</td>
+                                                    <td>05/02/2014</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3324</td>
+                                                    <td>10/21/2013</td>
+                                                    <td>3:03 PM</td>
+                                                    <td>$724.17</td>
+                                                </tr>
+                                         
+                                                <tr>
+                                                    <td>3319</td>
+                                                    <td>10/21/2013</td>
+                                                    <td>2:13 PM</td>
+                                                    <td>$943.45</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <!-- /.table-responsive -->
+                                </div>
+                                <!-- /.col-lg-4 (nested) -->
+                                <div class="col-lg-8">
+                                    <div id="morris-bar-chart"></div>
+                                </div>
+                                <!-- /.col-lg-8 (nested) -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
 	
 		    
 		</div><!-- /.page-content inset -->
