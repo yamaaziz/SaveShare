475a8bf83ba5416b778ed6b11e750d24c6ec5fdb
johannaingmar <jingmar_@hotmail.com>
2014-05-15 11:07:55 +0200

    15e maj

--- application/views/profile/extra.php
+++ application/views/profile/extra.php
@@ -8,21 +8,21 @@
 	<div class="btn-group">
 
 	<?php $index = 0; ?>
-	<?php $matches = 0; ?>
+	<?php $mismatches = 0; ?>
 	<?php if (count($following) == 0) { ?> <!--of the user is not following anyone-->
 			<a class="btn btn-primary btn-lg" href="<?php echo base_url(); ?>profile/follow"><i class="fa fa-heart"></i> Follow </a>
 	<?php } ?>
 	<?php if (count($following) != 0) { ?> <!--if the user is following anyone-->
 		<?php foreach (range(0, count($following)-1) as $whatever) { ?> <!--go through the list of followers-->
 			<?php if (array_values(array_values(array_values($following)[$index])[0])[0] != $var['username']) { ?> <!--if the username of the profile is in my followers list -->
-				<?php $matches = $matches + 1; ?>	
+				<?php $mismatches = $mismatches + 1; ?>	
 			<?php } ?>
 			<?php $index = $index + 1;?> 
 		<?php } ?>
-		<?php if (count($following) == $matches) { ?> <!--if not in the list-->
+		<?php if (count($following) == $mismatches) { ?> <!--if not in the list-->
 			<a class="btn btn-primary btn-lg" href="<?php echo base_url(); ?>profile/follow"><i class="fa fa-heart"></i> Follow </a>
 		<?php } ?>
-		<?php if (count($following) != $matches) { ?> <!--if in the list-->
+		<?php if (count($following) != $mismatches) { ?> <!--if in the list-->
 			<a class="btn btn-primary btn-lg" href="<?php echo base_url(); ?>profile/unfollow"><i class="fa fa-heart"></i> Unfollow </a>
 		<?php } ?>
 	<?php } ?>
